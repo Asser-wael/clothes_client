a271991451d73ba37c35a1aa1d57ee0f624943f1
Asser-wael <aserweal@gmail.com>
2026-07-28 00:45:44 +0300

    Add React project

--- src/pages/userPages/Products.jsx
+++ src/pages/userPages/Products.jsx
@@ -165,7 +165,7 @@ export default function Products() {
                 className="group flex w-full flex-row overflow-hidden rounded-2xl border border-[var(--color-border)] bg-[var(--color-card)] transition-all duration-200 hover:border-accent/40 hover:shadow-md lg:flex-col"
               >
                 {/* Image Container (مضبوط بالكامل ومستقر الأبعاد) */}
-                <div className="relative m-2 aspect-[4/5]  max-lg:h-[120px]   max-lg:w-[120px] shrink-0 cursor-pointer overflow-hidden rounded-xl bg-neutral-100 lg:m-0 lg:aspect-[4/3] lg:h-auto lg:w-full lg:rounded-none">
+                <div className="relative m-2 aspect-[4/5]  max-lg:h-[120px]   max-lg:w-[120px] shrink-0 cursor-pointer overflow-hidden rounded-xl bg-neutral-100 lg:m-0  h-full  lg:w-full lg:rounded-none">
                   <img
                     onClick={() => dispatch(setView(product))}
                     src={`${import.meta.env.VITE_API_URL}/uploads/${product.image}`}
